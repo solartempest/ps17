af42718ca0122e3b10817517e057ebe7c6f5fac4
solartempest <82990256+solartempest@users.noreply.github.com>
2021-04-23 20:10:18 -0400

    Add files via upload
    VIA keymaps

---
+++ keymaps/via/keymap.c
@@ -0,0 +1,124 @@
+/* Copyright
+ *   2021 solartempest
+ *   2021 QMK
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+#include "PS17.h"
+
+enum layer_names {
+    _LAYER0,
+    _LAYER1,
+    _LAYER2,
+    _LAYER3
+};
+
+    /*k10, k11, k12, k13, \
+      k20, k21, k22, k23, \
+      k30, k31, k32, 	  \
+      k40, k41, k42, k43, \
+      k50, 		k52		  \*/
+	  
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_LAYER0] = LAYOUT(
+		MO(1), KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS,
+        KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_PLUS,
+        KC_KP_4, KC_KP_5, KC_KP_6,
+        KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_ENTER,
+        KC_KP_0,          KC_KP_DOT
+    ),
+    [_LAYER1] = LAYOUT(
+        RESET, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,          KC_TRNS
+    ),
+    [_LAYER2] = LAYOUT(
+        RESET, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,          KC_TRNS
+    ),
+    [_LAYER3] = LAYOUT(
+        RESET, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,          KC_TRNS
+    ),
+};
+
+void matrix_init_user(void) {
+}
+
+void matrix_scan_user(void) {
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	return true;
+}
+
+void led_set_user(uint8_t usb_led) {
+
+	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
+		DDRD |= (1 << 0); PORTD &= ~(1 << 0);
+	} else {
+		DDRD &= ~(1 << 0); PORTD &= ~(1 << 0);
+	}
+
+	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
+		DDRD |= (1 << 5); PORTD &= ~(1 << 5);
+	} else {
+		DDRD &= ~(1 << 5); PORTD &= ~(1 << 5);
+	}
+
+	if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
+		DDRD |= (1 << 4); PORTD &= ~(1 << 4);
+	} else {
+		DDRD &= ~(1 << 4); PORTD &= ~(1 << 4);
+	}
+
+	if (usb_led & (1 << USB_LED_COMPOSE)) {
+		
+	} else {
+		
+	}
+
+	if (usb_led & (1 << USB_LED_KANA)) {
+		
+	} else {
+		
+	}
+
+}
+
+void keyboard_post_init_user(void) {
+  #ifdef RGBLIGHT_ENABLE
+    // Set up RGB effects on _only_ the third LED (index 2)
+    //rgblight_set_effect_range(2, 1);
+    // Set LED effects to breathing mode in a tealish blue color
+	rgblight_enable();
+    rgblight_sethsv_noeeprom(185, 255, 255);
+    rgblight_mode_noeeprom(RGBLIGHT_EFFECT_BREATHING + 2);
+
+    // Init the first two LEDs to a static color
+/*    setrgb(0, 0, 0, (LED_TYPE *)&led[0]);
+    setrgb(0, 0, 0, (LED_TYPE *)&led[1]);*/
+    rgblight_set();
+  #endif //RGBLIGHT_ENABLE
+}
