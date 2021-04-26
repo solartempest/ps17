a81827b80662b9f9b91e822b6f38316535f4993c
solartempest <82990256+solartempest@users.noreply.github.com>
2021-04-26 16:13:34 -0400

    Improved code for gradients and RGB Matrix.

--- keymaps/via/keymap.c
+++ keymaps/via/keymap.c
@@ -18,6 +18,9 @@
 
 #include QMK_KEYBOARD_H
 #include "PS17.h"
+#ifdef RGBLIGHT_ENABLE
+	extern rgblight_config_t rgblight_config; // To pull layer status for RGBLIGHT
+#endif
 
 enum layer_names {
     _LAYER0,
@@ -28,28 +31,32 @@ enum layer_names {
 	  
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LAYER0] = LAYOUT(
-		MO(1), KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS,
+		KC__VOLDOWN, KC__MUTE, KC__VOLUP,
+		KC_NUMLOCK, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS,
         KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_PLUS,
         KC_KP_4, KC_KP_5, KC_KP_6,
         KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_ENTER,
         KC_KP_0,          KC_KP_DOT
     ),
     [_LAYER1] = LAYOUT(
-        RESET, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS,          KC_TRNS
     ),
     [_LAYER2] = LAYOUT(
-        RESET, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, RESET, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS,          KC_TRNS
     ),
     [_LAYER3] = LAYOUT(
-        RESET, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, RESET, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -57,45 +64,113 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
-void matrix_init_user(void) {
-}
+void matrix_init_user(void) { //run when matrix is initiated, before all features init
+};
 
-void matrix_scan_user(void) {
-}
+void matrix_scan_user(void) { //run whenever user matrix is scanned
+};
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+bool process_record_user(uint16_t keycode, keyrecord_t *record) { //Actions to override existing key behaviours
 	return true;
-}
+};
 
-void led_set_user(uint8_t usb_led) {
-	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
-		DDRD |= (1 << 0); PORTD &= ~(1 << 0);
-	} else {
-		DDRD &= ~(1 << 0); PORTD &= ~(1 << 0);
-	}
-	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-		DDRD |= (1 << 5); PORTD &= ~(1 << 5);
-	} else {
-		DDRD &= ~(1 << 5); PORTD &= ~(1 << 5);
-	}
-	if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
-		DDRD |= (1 << 4); PORTD &= ~(1 << 4);
-	} else {
-		DDRD &= ~(1 << 4); PORTD &= ~(1 << 4);
-	}
-	if (usb_led & (1 << USB_LED_COMPOSE)) {
-	} else {
-	}
-	if (usb_led & (1 << USB_LED_KANA)) {
-	} else {
-	}
-}
 
-void keyboard_post_init_user(void) {
+void keyboard_post_init_user(void) {	//run as last task in keyboard init
+  #ifdef RGB_MATRIX_ENABLE
+    //NOTE 1: Layer lighting doesn't work in RGB matrix mode
+	//NOTE 2: VIA lighting tab doesn't work and will crash in RGB matrix mode
+	//NOTE 3: VIA layers doesn't seem to work properly in RGB matrix mode
+	//rgb_matrix_enable_noeeprom(); //turn on RGB matrix based on previous state
+	rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE); //set initial RGB matrix mode	
+	rgb_matrix_sethsv_noeeprom(58, 255, 80); //sets LED to green-yellow
+  #endif
   #ifdef RGBLIGHT_ENABLE
 	rgblight_enable();
-    rgblight_sethsv_noeeprom(185, 255, 255);
-    rgblight_mode_noeeprom(RGBLIGHT_EFFECT_BREATHING + 2);
-    rgblight_set();
-  #endif //RGBLIGHT_ENABLE
-}
+	rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_GRADIENT+8); //Set to static gradient 9. Looks great this way!
+	//rgblight_mode_noeeprom(RGBLIGHT_MODE_KNIGHT); //For LED mapping sequence testing or demo
+	rgblight_set();
+  #endif
+};
+
+
+#ifdef RGBLIGHT_ENABLE
+layer_state_t layer_state_set_user(layer_state_t state) {
+	switch (get_highest_layer(state)) {
+		//This custom layer lighting is similar to RGB Light Gradient 9, but customized to separate key and case underglow brightness
+		static uint16_t underglow_brightness = 140;
+		static uint16_t perkey_brightness = 80;
+		static uint16_t lastrow_brightness = 15; //for less shine-through at case edges
+		static uint16_t current_hue = 0; //hue calculated to be used
+		static uint16_t layer0_huestart = 56; //hue gradient starting colour - green/blue
+		static uint16_t layer0_hueincrement = 3; //hue gradient colour increment
+		static uint16_t layer1_huestart = 15; //hue gradient starting colour - orange/green
+		static uint16_t layer1_hueincrement = 2; //hue gradient colour increment
+		static uint16_t layer2_huestart = 135; //hue gradient starting colour - blue/purple
+		static uint16_t layer2_hueincrement = 3; //hue gradient colour increment
+		static uint16_t layer3_huestart = 220; //hue gradient starting colour - magenta/orange
+		static uint16_t layer3_hueincrement = 2; //hue gradient colour increment
+		
+		/*#define RGBLIGHT_LED_MAP LED_LAYOUT( \
+		1,                 0, \
+		3,                 2, \
+		9,   8, 7,  6,  5, 4, \
+			13, 12, 11, 10,   \
+		18, 17, 16, 15,    14,\
+			22, 21, 20, 19,   \
+		27, 26, 25, 24,    23 )*/
+		
+		case _LAYER0:
+			for (uint8_t i = 0; i < RGBLED_NUM; i++){
+				current_hue=i*layer0_hueincrement+layer0_huestart; //Determine the calculated hue
+				if(current_hue>255){current_hue=current_hue-255;}; //Roll over max hue of 256
+				if (i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 9 || i == 14 || i == 18 || i == 23 || i == 25 || i == 27) {	  
+				  rgblight_sethsv_at(current_hue,255,underglow_brightness,i);
+				} else if (i == 26 || i == 24 || i == 19 || i ==10) {
+				  rgblight_sethsv_at(current_hue,255,lastrow_brightness,i);
+				} else {
+				  rgblight_sethsv_at(current_hue,255,perkey_brightness,i);
+				};
+			};
+			break;
+		case _LAYER1:
+			for (uint8_t i = 0; i < RGBLED_NUM; i++){
+				current_hue=i*layer1_hueincrement+layer1_huestart; //Determine the calculated hue
+				if(current_hue>255){current_hue=current_hue-255;}; //Roll over max hue of 256
+				if (i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 9 || i == 14 || i == 18 || i == 23 || i == 25 || i == 27) {	  
+				  rgblight_sethsv_at(current_hue,255,underglow_brightness,i);
+				} else if (i == 26 || i == 24 || i == 19 || i ==10) {
+				  rgblight_sethsv_at(current_hue,255,lastrow_brightness,i);
+				} else {
+				  rgblight_sethsv_at(current_hue,255,perkey_brightness,i);
+				};
+			};
+			break;
+		case _LAYER2:
+			for (uint8_t i = 0; i < RGBLED_NUM; i++){
+				current_hue=i*layer2_hueincrement+layer2_huestart; //Determine the calculated hue
+				if(current_hue>255){current_hue=current_hue-255;}; //Roll over max hue of 256
+				if (i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 9 || i == 14 || i == 18 || i == 23 || i == 25 || i == 27) {	  
+				  rgblight_sethsv_at(current_hue,255,underglow_brightness,i);
+				} else if (i == 26 || i == 24 || i == 19 || i ==10) {
+				  rgblight_sethsv_at(current_hue,255,lastrow_brightness,i);
+				} else {
+				  rgblight_sethsv_at(current_hue,255,perkey_brightness,i);
+				};
+			};
+			break;
+		case _LAYER3:
+			for (uint8_t i = 0; i < RGBLED_NUM; i++){
+				current_hue=i*layer3_hueincrement+layer3_huestart; //Determine the calculated hue
+				if(current_hue>255){current_hue=current_hue-255;}; //Roll over max hue of 256
+				if (i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 9 || i == 14 || i == 18 || i == 23 || i == 25 || i == 27) {	  
+				  rgblight_sethsv_at(current_hue,255,underglow_brightness,i);
+				} else if (i == 26 || i == 24 || i == 19 || i ==10) {
+				  rgblight_sethsv_at(current_hue,255,lastrow_brightness,i);
+				} else {
+				  rgblight_sethsv_at(current_hue,255,perkey_brightness,i);
+				};
+			};
+		}
+	return state;
+	};
+#endif
