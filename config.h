cdd101823933147b88bb0f06633ba8a251741f8e
solartempest <82990256+solartempest@users.noreply.github.com>
2021-04-26 16:12:10 -0400

    Matrix RGB Code + RGB Light Remapping Complete

--- config.h
+++ config.h
@@ -17,51 +17,146 @@
  */
 
 #pragma once
-
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
+// USB Device descriptor parameter 
 #define VENDOR_ID       0x414B // "AK"
 #define PRODUCT_ID      0x0015
 #define DEVICE_VER      0x0002 // Ver2
 #define MANUFACTURER    ATLANTIS
 #define PRODUCT         PS17
 
-/* key matrix size */
-#define MATRIX_ROWS 5
+//#define ENCODER_RESOLUTION 2 // default is 4
+
+// key matrix size 
+#ifdef RGBLIGHT_ENABLE 
+	#define MATRIX_ROWS 6
+#endif
+#ifdef RGB_MATRIX_ENABLE 
+	#define MATRIX_ROWS 11 //Numpad actually has 6 rows. 5 rows were added for underglow RGB matrix to work together
+#endif
 #define MATRIX_COLS 4
 
-    /*k10, k11, k12, k13 \
+    /*k00, k01, k12      \
+      k10, k11, k12, k13 \
       k20, k21, k22, k23 \
       k30, k31, k32, ___ \
       k40, k41, k42, k43 \
       k50, ___, k52, ___  \*/
 
-/* Planck PCB default pin-out */
-#define MATRIX_ROW_PINS { B4, B5, B6, C6, C7 }
+// Rotary encoder has built in debounce circuit
+#define ENCODERS_PAD_A { D1 }
+#define ENCODERS_PAD_B { D2 }
+
+#define MATRIX_ROW_PINS { F0, B4, B5, B6, C6, C7 } //Rotary encoder SPST is on F0,F7
 #define MATRIX_COL_PINS { F6, F7, D3, D6 }
 #define UNUSED_PINS
 
-//#define ENCODER_RESOLUTION 2 // default is 4
-
-/* COL2ROW or ROW2COL */
+// COL2ROW or ROW2COL
 #define DIODE_DIRECTION COL2ROW
 
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
-#define RGB_DI_PIN B3
-#ifdef RGB_DI_PIN
-	#define RGBLIGHT_ANIMATIONS
-	#define RGBLED_NUM 28 // 17 key + 11 underglow
+// For LED status indicators
+#define LED_NUM_LOCK_PIN D0
+#define LED_CAPS_LOCK_PIN D5
+#define LED_SCROLL_LOCK_PIN D4
+#define LED_PIN_ON_STATE 0
+
+#define RGB_DI_PIN B7
+#ifdef RGBLIGHT_ENABLE
+	#define RGBLED_NUM 28 // 17 key + 11 underglow for RGBLIGHT
 	#define RGBLIGHT_HUE_STEP 8
 	#define RGBLIGHT_SAT_STEP 8
 	#define RGBLIGHT_VAL_STEP 8
-	#define RGBLIGHT_LIMIT_VAL 150 //  brightness level max is 255
+	#define RGBLIGHT_LIMIT_VAL 200 // brightness level max is 255
 	#define RGBLIGHT_SLEEP //  if defined, RGB lighting off when host is sleeping
+	#define RGBLIGHT_ANIMATIONS
+
+	//RGB LED Conversion macro from physical array to electric array
+	//First section is the LED matrix, second section is the electrical wiring order, and the third section is the desired mapping
+	#define LED_LAYOUT( \
+		L00,                     L05,  \
+		L10,                     L15,  \
+		L20, L21, L22, L23, L24, L25,  \
+		     L31, L32, L33, L34,       \
+		L40, L41, L42, L43,      L45,  \
+		     L51, L52, L53, L54,       \
+		L60, L61, L62, L63,      L65 ) \
+	  { \
+		L21,L22,L23,L24,L31,L32,L33,L34,L41,L42,L43,L51,L52,L53,L54,L61,L63,L60,L40,L20,L10,L00,L05,L15,L25,L45,L65,L62, \
+	  }
+	//RGB LED logical order map
+	//Right->Left, Top->Bottom
+	#define RGBLIGHT_LED_MAP LED_LAYOUT( \
+		1,                  0, \
+		3,                  2, \
+		9,   8,  7,  6,  5, 4, \
+		    13, 12, 11, 10,    \
+		18, 17, 16, 15,    14, \
+		    22, 21, 20, 19,    \
+		27, 26, 25, 24,    23  )
+		
+	//Top->Bottom, Right->Left (Alternate mapping)
+	/*#define RGBLIGHT_LED_MAP LED_LAYOUT( \
+		23,                0,  \
+		24,                1,  \
+		25, 18, 13, 8, 5,  2,  \
+		    19, 14, 9, 6,      \
+		26, 20, 15, 10,    3,  \
+		    21, 16, 11, 7,     \
+		27, 22, 17, 12,    4 )*/
+		
+	//Top->Bottom, Left->Right (Alternate mapping)
+	/*#define RGBLIGHT_LED_MAP LED_LAYOUT( \
+		0,                23,  \
+		1,                24,  \
+		2, 5, 10, 15, 20, 25,  \
+		   6, 11, 16, 21,      \
+		3, 7, 12, 17,     26,  \
+		   8, 13, 18, 22,      \
+		4, 9, 14, 19,     27 )*/
+
+#endif
+
+#ifdef RGB_MATRIX_ENABLE // For dynamic RGB Matrix effects
+	#define DRIVER_LED_TOTAL 28 // The number of LEDs connected for matrix, 17 key + 11 underglow
+	#define RGB_MATRIX_CENTER { 126, 126 };
+	#define RGB_MATRIX_KEYPRESSES
+	#define DISABLE_RGB_MATRIX_ALPHAS_MODS //Remove effects not desired
+	#define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+	#define DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+	#define DISABLE_RGB_MATRIX_BAND_SAT
+	#define DISABLE_RGB_MATRIX_BAND_VAL
+	#define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+	#define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+	#define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+	#define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+	#define DISABLE_RGB_MATRIX_CYCLE_ALL
+	#define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
+	#define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+	#define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+	#define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL
+	#define DISABLE_RGB_MATRIX_CYCLE_SPIRAL
+	#define DISABLE_RGB_MATRIX_DUAL_BEACON
+	#define DISABLE_RGB_MATRIX_RAINBOW_BEACON
+	#define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+	#define DISABLE_RGB_MATRIX_RAINDROPS
+	#define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+	#define DISABLE_RGB_MATRIX_HUE_BREATHING
+	#define DISABLE_RGB_MATRIX_HUE_PENDULUM
+	#define DISABLE_RGB_MATRIX_HUE_WAVE
+	#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+	#define DISABLE_RGB_MATRIX_SOLID_REACTIVE
+	#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+	#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+	#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+	#define DISABLE_RGB_MATRIX_SPLASH
+	#define DISABLE_RGB_MATRIX_MULTISPLASH
+	#define DISABLE_RGB_MATRIX_SOLID_SPLASH
+	#define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
 #endif
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
+#define DEBOUNCE 0
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
