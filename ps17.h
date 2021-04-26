a468c3a81b3339b0ca10de8320c6859cb6bf5190
solartempest <82990256+solartempest@users.noreply.github.com>
2021-04-26 16:14:05 -0400

    Added keymap for RGB matrix of case underglow.

--- ps17.h
+++ ps17.h
@@ -17,19 +17,45 @@
  */
 
 #pragma once
-
 #include "quantum.h"
 
-#define LAYOUT( 		\
-	K10, K11, K12, K13, \
-	K20, K21, K22, K23, \
-	K30, K31, K32,      \
-	K40, K41, K42, K43, \
-	K50,	  K52       \
-) { \
-	{ K10,   K11,   K12,   K13 }, 	\
-	{ K20,   K21,   K22,   K23 },   \
-	{ K30,   K31,   K32,   KC_NO }, \
-	{ K40,   K41,   K42,   K43 },   \
-	{ K50,   KC_NO, K52,   KC_NO }, \
-}
+#ifdef RGBLIGHT_ENABLE
+	#define LAYOUT( 		\
+		K00, K01, K02,      \
+		K10, K11, K12, K13, \
+		K20, K21, K22, K23, \
+		K30, K31, K32,      \
+		K40, K41, K42, K43, \
+		K50,	  K52       \
+	) { \
+		{ K00,   K01,   K02,   KC_NO },	\
+		{ K10,   K11,   K12,   K13 }, 	\
+		{ K20,   K21,   K22,   K23 },   \
+		{ K30,   K31,   K32,   KC_NO }, \
+		{ K40,   K41,   K42,   K43 },   \
+		{ K50,   KC_NO, K52,   KC_NO }, \
+	}
+#endif
+
+#ifdef RGB_MATRIX_ENABLE // Five empty rows of keys were added for RGB Matrix purposes. You must map to a key otherwise it will not work.
+	#define LAYOUT( 		\
+		K00, K01, K02,      \
+		K10, K11, K12, K13, \
+		K20, K21, K22, K23, \
+		K30, K31, K32,      \
+		K40, K41, K42, K43, \
+		K50,	  K52       \
+	) { \
+		{ K00,   K01,   K02,   KC_NO },	\
+		{ K10,   K11,   K12,   K13 }, 	\
+		{ K20,   K21,   K22,   K23 },   \
+		{ K30,   K31,   K32,   KC_NO }, \
+		{ K40,   K41,   K42,   K43 },   \
+		{ K50,   KC_NO, K52,   KC_NO }, \
+		{ KC_NO, KC_NO, KC_NO, KC_NO }, \
+		{ KC_NO, KC_NO, KC_NO, KC_NO }, \
+		{ KC_NO, KC_NO, KC_NO, KC_NO }, \
+		{ KC_NO, KC_NO, KC_NO, KC_NO }, \
+		{ KC_NO, KC_NO, KC_NO, KC_NO }, \
+	}
+#endif
