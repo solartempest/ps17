9fd56de5a9018ba5b5cac01e0c44f5540546581a
solartempest <82990256+solartempest@users.noreply.github.com>
2021-06-09 10:12:18 -0400

    Added mouse keys support back.

--- rules.mk
+++ rules.mk
@@ -9,7 +9,7 @@ BOOTLOADER = atmel-dfu
 #
 
 BOOTMAGIC_ENABLE = no      # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no       # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes      # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
